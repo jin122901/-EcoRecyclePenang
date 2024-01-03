ed17b423f919906382dc9b7675b6870d14ed1277
Tommy Tan <tanjunlin0215@outlook.com>
2024-01-03 14:02:52 +0800

    add on databases file into system

---
+++ assets/databases/dbconn.php
@@ -0,0 +1,9 @@
+<?php 
+    $servername = "localhost"; //localhost or livehost
+    $dbusername = "root"; //username db
+    $dbpwd = ""; //password db
+    $dbname = "ecorecyclepenang"; //databases name
+
+    //Create connection for new databases
+    $connDB = mysqli_connect($servername,$dbusername,$dbpwd);
+?>
