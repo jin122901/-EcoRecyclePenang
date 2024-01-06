ff2458b3411cc0c9cbc6c511c877610ef048fe07
jin122901 <jiajin1068@gmail.com>
2024-01-06 22:25:08 +0800

    update

--- navbar.php
+++ navbar.php
@@ -14,7 +14,7 @@
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
                             <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
                             <li class="nav-item"><a class="nav-link" href="recycle.php">Recycle</a></li>
-                            <li class="nav-item"><a class="nav-link" href="projects.html">Projects</a></li>
+                            <li class="nav-item"><a class="nav-link" href="projects.php">Projects</a></li>
 							<li class="nav-item"><a class="nav-link" href="reward.php">Rewards</a></li>
                             <li class="nav-item"><a class="nav-link" href="loginPage.php">Contact</a></li>
                             <li class="nav-item"><a class="nav-link" href="loginPage.php">Login</a></li>
