ce6625c0e9f66ead3b5ac5c183a3f1489ae7ce61
jin122901 <jiajin1068@gmail.com>
2024-01-06 21:02:49 +0800

    admin nav

---
+++ Admin/navbar.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<!-- Navigation-->
+            <nav class="navbar navbar-expand-lg navbar-light bg-white py-3">
+                <div class="container px-5">
+					<img src="assets/recycle.ico" width="30px" height="30px" alt="Icon Description"> &emsp;
+                    <a class="navbar-brand" href="index.php"><span class="fw-bolder text-primary">EcoRecyclePenang</span></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
+                            <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                            <li class="nav-item"><a class="nav-link" href="recycle.php">Recycle</a></li>
+                            <li class="nav-item"><a class="nav-link" href="projects.html">Projects</a></li>
+							<li class="nav-item"><a class="nav-link" href="reward.php">Rewards</a></li>
+                            <li class="nav-item"><a class="nav-link" href="loginPage.php">Contact</a></li>
+                            <li class="nav-item"><a class="nav-link" href="loginPage.php">Login</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+<body>
+</body>
+</html>
